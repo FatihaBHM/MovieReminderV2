b0b07c13edd5a5531fd647ec286e29e915cd4908
DESKTOP-3RQBHHG\admin <fatiha.benou-halilma@epita.fr>
2024-03-21 10:10:56 +0100

    MovieController

---
+++ exposition/src/main/java/org/lafabrique_epita/exposition/api/media/MovieController.java
@@ -0,0 +1,39 @@
+package org.lafabrique_epita.exposition.api.media;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.Valid;
+import org.lafabrique_epita.application.service.media.MovieServiceImpl;
+import org.lafabrique_epita.domain.entities.MovieEntity;
+import org.lafabrique_epita.domain.entities.UserEntity;
+import org.lafabrique_epita.exposition.dto.movie.MoviePostDto;
+import org.lafabrique_epita.exposition.dto.movie.MoviePostDtoMapper;
+import org.lafabrique_epita.exposition.dto.movie.MoviePostDtoResponseMapper;
+import org.lafabrique_epita.exposition.dto.user.RegisterDto;
+import org.lafabrique_epita.exposition.dto.user.RegisterDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+//@RequestMapping("/movies")
+public class MovieController {
+
+    private final MovieServiceImpl movieService;
+    private final ObjectMapper mapper;
+
+
+    public MovieController(MovieServiceImpl movieService, ObjectMapper mapper) {
+        this.movieService = movieService;
+        this.mapper = mapper;
+
+    }
+
+    @PostMapping("/movies")
+    public ResponseEntity<MovieEntity> getFrontMovie(@Valid @RequestBody MoviePostDto moviePostDto) {
+        MovieEntity movie = MoviePostDtoMapper.convertToMovieEntity(moviePostDto);
+       MovieEntity movieEntity = movieService.save(movie);
+        return ResponseEntity.ok(movieEntity);
+    }
+
+
+}
