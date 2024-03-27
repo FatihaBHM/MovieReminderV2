cc096b676618bf9967ac70c241fa2fa0d5d5f074
user_name <example.gmail.com>
2024-03-27 10:23:43 +0100

    :recycle: refactor: Supprime les serveurs spécifiques de la configuration Swagger

--- exposition/src/main/java/org/lafabrique_epita/exposition/configuration/SwaggerConfig.java
+++ exposition/src/main/java/org/lafabrique_epita/exposition/configuration/SwaggerConfig.java
@@ -6,33 +6,15 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Value("${openapi.dev-url}")
-    private String openApiDevUrl;
-
-    @Value("${openapi.prod-url}")
-    private String openApiProdUrl;
-
     @Bean
     OpenAPI customOpenApi() {
-        Server devServer = new Server();
-        devServer.setUrl(openApiDevUrl);
-        devServer.setDescription("Development server");
-
-        Server prodServer = new Server();
-        prodServer.setUrl(openApiProdUrl);
-        prodServer.setDescription("Production server");
-
 
         return new OpenAPI()
                 .info(new Info()
@@ -51,8 +33,7 @@ public class SwaggerConfig {
                                                 .scheme("bearer")
                                                 .bearerFormat("JWT")
 
-                                ))
-                .servers(List.of(devServer, prodServer));
+                                ));
     }
 
     @Bean
