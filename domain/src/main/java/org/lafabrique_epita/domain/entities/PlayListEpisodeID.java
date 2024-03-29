1240ede3920b8f3c4095bcc17a7ca4747b15362a
user_name <example.gmail.com>
2024-03-29 11:01:46 +0100

    :recycle: refactor: Renomme `PlayListTvEntity` en `PlayListEpisodeEntity`

--- domain/src/main/java/org/lafabrique_epita/domain/entities/PlayListEpisodeID.java
+++ domain/src/main/java/org/lafabrique_epita/domain/entities/PlayListEpisodeID.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
-public class PlayListTvID implements Serializable {
+public class PlayListEpisodeID implements Serializable {
 
     @Column(name = "episode_id")
     private Long episodeId;
--- infrastructure/src/main/java/org/lafabrique_epita/infrastructure/playlist_tv/PlayListTvJPARepositoryPort.java
+++ infrastructure/src/main/java/org/lafabrique_epita/infrastructure/playlist_tv/PlayListTvJPARepositoryPort.java
@@ -1,7 +1,7 @@
 package org.lafabrique_epita.infrastructure.playlist_tv;
 
 import org.lafabrique_epita.domain.entities.EpisodeEntity;
-import org.lafabrique_epita.domain.entities.PlayListTvEntity;
+import org.lafabrique_epita.domain.entities.PlayListEpisodeEntity;
 import org.lafabrique_epita.domain.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.util.Optional;
 
-public interface PlayListTvJPARepositoryPort extends JpaRepository<PlayListTvEntity, Long> {
+public interface PlayListTvJPARepositoryPort extends JpaRepository<PlayListEpisodeEntity, Long> {
 
-    Optional<PlayListTvEntity> findByEpisodeIdAndUserId(Long episodeId, Long userId);
+    Optional<PlayListEpisodeEntity> findByEpisodeIdAndUserId(Long episodeId, Long userId);
 
-    @Query("SELECT episode FROM PlayListTvEntity WHERE user = :user")
+    @Query("SELECT episode FROM PlayListEpisodeEntity WHERE user = :user")
     List<EpisodeEntity> findEpisodesByUserId(@PathVariable UserEntity user);
 
     boolean existsByEpisodeIdAndUserId(Long serieId, Long userId);
